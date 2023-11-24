aafbcc0893e2f01963b93c889995c96e6c832dce
MdShimulMahmud <shimulmahmud18@gmail.com>
2023-11-24 11:02:21 +0600

    redux toolkit video gallery link updated

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL:
-    "http://localhost:9000" ||
-    "https://redux-toolkit-video-gallery-json-server.onrender.com",
+  baseURL: "https://redux-toolkit-video-gallery-json-server.onrender.com",
 });
 
 export default axiosInstance;
